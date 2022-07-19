8b8061465dbff5d4db3fab3af04bbeaf48767d50
Hillol-Talukdar <talukdarhillol@gmail.com>
2022-07-19 17:42:50 +0600

    Implemented First page. And intent to all book activity page

--- app/src/main/java/com/example/mylibrary/MainActivity.java
+++ app/src/main/java/com/example/mylibrary/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.mylibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSeeAll;
+    private Button btnCurrentlyReading;
+    private Button btnWantToRead;
+    private Button btnAlreadyRead;
+    private Button btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initWidgets();
+        setOnClickListeners();
+    }
+
+    private void setOnClickListeners () {
+        btnSeeAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AllBooksActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initWidgets() {
+        btnSeeAll = (Button) findViewById(R.id.btnAllBooks);
+        btnCurrentlyReading = (Button) findViewById(R.id.btnCurrent);
+        btnWantToRead = (Button) findViewById(R.id.btnWantTo);
+        btnAlreadyRead = (Button) findViewById(R.id.btnAlreadyRead);
+        btnAbout = (Button) findViewById(R.id.btnAbout);
     }
 }
